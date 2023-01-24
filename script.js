d5bd19e3274bf0104a46ca55ef10b21362919253
raheel1435 <shanir_@hotmail.com>
2023-01-24 12:11:47 +0000

    putting flying element properties and DOMreact

---
+++ script.js
@@ -0,0 +1,35 @@
+let move_speed = 3, grativy = 0.5;
+let flying = document.querySelector('.flying');
+let img = document.getElementById('flying-1');
+
+
+// putting flying element properties
+let flying_props = flying.getBoundingClientRect();
+
+// This method returns DOMReact -> top, right, bottom, left, x, y, width and height
+let background = document.querySelector('.background').getBoundingClientRect();
+
+let score_val = document.querySelector('.score_val');
+let message = document.querySelector('.message');
+let score_title = document.querySelector('.score_title');
+
+let game_state = 'Start';
+img.style.display = 'none';
+message.classList.add('messageStyle');
+
+document.addEventListener('keydown', (e) => {
+
+    if(e.key == 'Enter' && game_state != 'Play'){
+        document.querySelectorAll('.pipe_sprite').forEach((e) => {
+            e.remove();
+        });
+        img.style.display = 'block';
+        flying.style.top = '40vh';
+        game_state = 'Play';
+        message.innerHTML = '';
+        score_title.innerHTML = 'Score : ';
+        score_val.innerHTML = '0';
+        message.classList.remove('messageStyle');
+        play();
+    }
+});
