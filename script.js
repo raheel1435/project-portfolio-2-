ea1d336dc1bdcc81c20e8f57a82371ec9fbdee1b
raheel1435 <shanir_@hotmail.com>
2023-01-26 00:03:08 +0000

    putting sound controller and few more changings in all code

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 let move_speed = 3, grativy = 0.5;
-let flying = document.querySelector('.flying');
-let img = document.getElementById('flying-1');
+let bird = document.querySelector('.bird');
+let img = document.getElementById('bird-1');
+let sound_point = new Audio('sounds effect/point.mp3');
+let sound_die = new Audio('sounds effect/die.mp3');
 
+// setting sound controller
 
-// putting flying element properties
-let flying_props = flying.getBoundingClientRect();
+var audio = document.getElementById('audio');
+var playPauseBTN = document.getElementById('playPauseBTN');
+var count = 0;
+
+function playPause(){
+    if(count == 0){
+        count = 1;
+        audio.play();
+        playPauseBTN.innerHTML = "Pause ⏸";
+    }else{
+        count = 0;
+        audio.pause();
+        playPauseBTN.innerHTML = "Play ►";
+    }
+
+}
+
+// getting bird element properties
+let bird_props = bird.getBoundingClientRect();
 
 // This method returns DOMReact -> top, right, bottom, left, x, y, width and height
 let background = document.querySelector('.background').getBoundingClientRect();
@@ -18,13 +38,13 @@ img.style.display = 'none';
 message.classList.add('messageStyle');
 
 document.addEventListener('keydown', (e) => {
-
+    
     if(e.key == 'Enter' && game_state != 'Play'){
-        document.querySelectorAll('.pipe_sprite').forEach((e) => {
+        document.querySelectorAll('.obstacle_sprite').forEach((e) => {
             e.remove();
         });
         img.style.display = 'block';
-        flying.style.top = '40vh';
+        bird.style.top = '40vh';
         game_state = 'Play';
         message.innerHTML = '';
         score_title.innerHTML = 'Score : ';
@@ -38,27 +58,27 @@ function play(){
     function move(){
         if(game_state != 'Play') return;
 
-        let pipe_sprite = document.querySelectorAll('.pipe_sprite');
-        pipe_sprite.forEach((element) => {
-            let pipe_sprite_props = element.getBoundingClientRect();
-            flying_props = flying.getBoundingClientRect();
+        let obstacle_sprite = document.querySelectorAll('.obstacle_sprite');
+        obstacle_sprite.forEach((element) => {
+            let obstacle_sprite_props = element.getBoundingClientRect();
+            bird_props = bird.getBoundingClientRect();
 
-            if(pipe_sprite_props.right <= 0){
+            if(obstacle_sprite_props.right <= 0){
                 element.remove();
             }else{
-                if(flying_props.left < pipe_sprite_props.left + pipe_sprite_props.width && flying_props.left + flying_props.width > pipe_sprite_props.left && flying_props.top < pipe_sprite_props.top + pipe_sprite_props.height && flying_props.top + flying_props.height > pipe_sprite_props.top){
+                if(bird_props.left < obstacle_sprite_props.left + obstacle_sprite_props.width && bird_props.left + bird_props.width > obstacle_sprite_props.left && bird_props.top < obstacle_sprite_props.top + obstacle_sprite_props.height && bird_props.top + bird_props.height > obstacle_sprite_props.top){
                     game_state = 'End';
-                    message.innerHTML = 'Game Over'.fontcolor('red') + '<br>Press Enter To Restart';
+                    message.innerHTML = 'Game Over'.fontcolor('white') + '<br>Press Enter To Restart';
                     message.classList.add('messageStyle');
                     img.style.display = 'none';
                     
                     return;
                 }else{
-                    if(pipe_sprite_props.right < flying_props.left && pipe_sprite_props.right + move_speed >= flying_props.left && element.increase_score == '1'){
+                    if(obstacle_sprite_props.right < bird_props.left && obstacle_sprite_props.right + move_speed >= bird_props.left && element.increase_score == '1'){
                         score_val.innerHTML =+ score_val.innerHTML + 1;
-                        sound_point.play();
+                        
                     }
-                    element.style.left = pipe_sprite_props.left - move_speed + 'px';
+                    element.style.left = obstacle_sprite_props.left - move_speed + 'px';
                 }
             }
         });
@@ -66,72 +86,63 @@ function play(){
     }
     requestAnimationFrame(move);
 
-    let flying_dy = 0;
+    let bird_dy = 0;
     function apply_gravity(){
         if(game_state != 'Play') return;
-        flying_dy = flying_dy + grativy;
+        bird_dy = bird_dy + grativy;
         document.addEventListener('keydown', (e) => {
             if(e.key == 'ArrowUp' || e.key == ' '){
-                img.src = 'images/flying.png';
-                flying_dy = -7.6;
+                img.src = 'images/Bird-2.png';
+                bird_dy = -7.6;
             }
         });
-
         document.addEventListener('keyup', (e) => {
             if(e.key == 'ArrowUp' || e.key == ' '){
-                img.src = 'images/flying.png';
+                img.src = 'images/Bird.png';
             }
         });
-
-        if(flying_props.top <= 0 || flying_props.bottom >= background.bottom){
+        
+        if(bird_props.top <= 0 || bird_props.bottom >= background.bottom){
             game_state = 'End';
             message.style.left = '28vw';
             window.location.reload();
             message.classList.remove('messageStyle');
             return;
         }
-        flying.style.top = flying_props.top + flying_dy + 'px';
-        flying_props = flying.getBoundingClientRect();
+        bird.style.top = bird_props.top + bird_dy + 'px';
+        bird_props = bird.getBoundingClientRect();
         requestAnimationFrame(apply_gravity);
     }
     requestAnimationFrame(apply_gravity);
 
-    let pipe_seperation = 0;
+    let obstacle_seperation = 0;
 
-    let pipe_gap = 35;
+    let obstacle_gap = 35;
 
-    function create_pipe(){
+    function create_obstacle(){
         if(game_state != 'Play') return;
 
-        if(pipe_seperation > 115){
-            pipe_seperation = 0;
+        if(obstacle_seperation > 115){
+            obstacle_seperation = 0;
 
-            let pipe_posi = Math.floor(Math.random() * 43) + 8;
-            let pipe_sprite_inv = document.createElement('div');
-            pipe_sprite_inv.className = 'pipe_sprite';
-            pipe_sprite_inv.style.top = pipe_posi - 70 + 'vh';
-            pipe_sprite_inv.style.left = '100vw';
+            let obstacle_posi = Math.floor(Math.random() * 43) + 8;
+            let obstacle_sprite_inv = document.createElement('div');
+            obstacle_sprite_inv.className = 'obstacle_sprite';
+            obstacle_sprite_inv.style.top = obstacle_posi - 70 + 'vh';
+            obstacle_sprite_inv.style.left = '100vw';
 
-            document.body.appendChild(pipe_sprite_inv);
-            let pipe_sprite = document.createElement('div');
-            pipe_sprite.className = 'pipe_sprite';
-            pipe_sprite.style.top = pipe_posi + pipe_gap + 'vh';
-            pipe_sprite.style.left = '100vw';
-            pipe_sprite.increase_score = '1';
+            document.body.appendChild(obstacle_sprite_inv);
+            let obstacle_sprite = document.createElement('div');
+            obstacle_sprite.className = 'obstacle_sprite';
+            obstacle_sprite.style.top = obstacle_posi + obstacle_gap + 'vh';
+            obstacle_sprite.style.left = '100vw';
+            obstacle_sprite.increase_score = '1';
 
-            document.body.appendChild(pipe_sprite);
+            document.body.appendChild(obstacle_sprite);
         }
-        pipe_seperation++;
-        requestAnimationFrame(create_pipe);
+        obstacle_seperation++;
+        requestAnimationFrame(create_obstacle);
     }
-    requestAnimationFrame(create_pipe);
+    requestAnimationFrame(create_obstacle);
 }
-window.onload = function() {
-    slider = document. querySelector(".slider input");
-    slider.oninput = function() {
-        progressBar = document.querySelector(".slider progress");
-        progressBar.value = slider.value;
-        sliderValue = document.querySelector(".sliderValue");
-        sliderValue.innerHTML = slider.value;
-    }
-}
+
